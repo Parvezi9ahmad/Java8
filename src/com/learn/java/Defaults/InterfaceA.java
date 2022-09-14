a43afaf1ccb2bfec253a64997d8009a76382c434
parvez Ahmad <parvezi9ahmad@gmail.com>
2022-09-14 18:04:01 +0530

    second commit for summary

--- src/com/learn/java/Defaults/InterfaceA.java
+++
@@ -1,7 +0,0 @@
-package com.learn.java.Defaults;
-
-public interface InterfaceA {
-      default void methodA() {
-    	  System.out.println("Inside Method A"+InterfaceA.class);
-      }
-}
--- src/com/learn/java/Defaults/InterfaceD.java
+++
@@ -1,7 +0,0 @@
-package com.learn.java.Defaults;
-
-public interface InterfaceD {
-	default void methodA() {
-		System.out.println("Inside methodA"+InterfaceD.class);
-	}
-}
